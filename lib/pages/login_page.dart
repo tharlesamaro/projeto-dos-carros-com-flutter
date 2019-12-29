4ca3e32804fcd80de51fcf2474a81396c9df6b0d
Tharles Amaro <tharlesamaro@gmail.com>
2019-12-29 16:20:00 -0300

    Validator para validação de formulários

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
   final _controllerLogin = TextEditingController();
   final _controllerSenha = TextEditingController();
 
@@ -16,31 +18,49 @@ class LoginPage extends StatelessWidget {
   }
 
   _body() {
-    return Container(
-      padding: EdgeInsets.all(16),
-      child: ListView(
-        children: <Widget>[
-          _textFormField("Login", "Digite o login",
-              controller: _controllerLogin),
-          SizedBox(
-            height: 10,
-          ),
-          _textFormField("Senha", "Digite a senha",
-              isPassword: true, controller: _controllerSenha),
-          SizedBox(
-            height: 20,
-          ),
-          _button("Login", _onClickLogin),
-        ],
+    return Form(
+      key: _formKey,
+      child: Container(
+        padding: EdgeInsets.all(16),
+        child: ListView(
+          children: <Widget>[
+            _textFormField(
+              "Login",
+              "Digite o login",
+              controller: _controllerLogin,
+              validator: _validateLogin,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            _textFormField(
+              "Senha",
+              "Digite a senha",
+              isPassword: true,
+              controller: _controllerSenha,
+              validator: _validateSenha,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            _button("Login", _onClickLogin),
+          ],
+        ),
       ),
     );
   }
 
-  _textFormField(String label, String hint,
-      {bool isPassword = false, TextEditingController controller}) {
+  _textFormField(
+    String label,
+    String hint, {
+    bool isPassword = false,
+    TextEditingController controller,
+    FormFieldValidator<String> validator,
+  }) {
     return TextFormField(
       controller: controller,
       obscureText: isPassword,
+      validator: validator,
       style: TextStyle(
         fontSize: 25,
         color: Colors.blue,
@@ -77,9 +97,34 @@ class LoginPage extends StatelessWidget {
   }
 
   void _onClickLogin() {
+    bool formValidationContainsErrors = ! _formKey.currentState.validate();
+
+    if (formValidationContainsErrors) {
+      return;
+    }
+
     String login = _controllerLogin.text;
     String senha = _controllerSenha.text;
 
     print("Login: $login, Senha: $senha");
+  }
+
+  String _validateLogin(String value) {
+    if (value.isEmpty) {
+      return "Campo login é obrigatório";
+    }
+    return null;
+  }
+
+  String _validateSenha(String value) {
+    if (value.isEmpty) {
+      return "Campo senha é obrigatório";
+    }
+
+    if (value.length < 4) {
+      return "Digite pelo menos 4 caracteres";
+    }
+
+    return null;
   }
 }
