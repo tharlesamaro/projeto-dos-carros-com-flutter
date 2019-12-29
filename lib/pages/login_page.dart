675a66e1f1969bf55ab402144a6622d8f3235859
Tharles Amaro <tharlesamaro@gmail.com>
2019-12-29 14:38:51 -0300

    Iniciando o layout do formulário de Login com TextFormField

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -14,7 +14,33 @@ class LoginPage extends StatelessWidget {
 
   _body() {
     return Container(
-
+      padding: EdgeInsets.all(16),
+      child: ListView(
+        children: <Widget>[
+          Text("Login"),
+          TextField(),
+          SizedBox(height: 20,),
+          Text("Senha"),
+          TextFormField(
+            obscureText: true,
+          ),
+          SizedBox(height: 20,),
+          Container(
+            height: 46,
+            child: RaisedButton(
+              color: Colors.blue,
+              child: Text(
+                "Login",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                ),
+              ),
+              onPressed: () {},
+            ),
+          )
+        ],
+      ),
     );
   }
 }
