63ac87f2da5649e092923b2336659a5267134997
Tharles Amaro <tharlesamaro@gmail.com>
2019-12-29 15:12:32 -0300

    Criando widgets com métodos

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -17,62 +17,55 @@ class LoginPage extends StatelessWidget {
       padding: EdgeInsets.all(16),
       child: ListView(
         children: <Widget>[
-          TextFormField(
-            style: TextStyle(
-              fontSize: 25,
-              color: Colors.blue,
-            ),
-            decoration: InputDecoration(
-              labelText: "Login",
-              labelStyle: TextStyle(
-                fontSize: 25,
-                color: Colors.grey,
-              ),
-              hintText: "Digite o login",
-              hintStyle: TextStyle(
-                fontSize: 16,
-              ),
-            ),
-          ),
+          _textFormField("Login", "Digite o login"),
           SizedBox(
             height: 10,
           ),
-          TextFormField(
-            style: TextStyle(
-              fontSize: 25,
-              color: Colors.blue,
-            ),
-            obscureText: true,
-            decoration: InputDecoration(
-              labelText: "Senha",
-              labelStyle: TextStyle(
-                fontSize: 25,
-                color: Colors.grey,
-              ),
-              hintText: "Digite a senha",
-              hintStyle: TextStyle(
-                fontSize: 16,
-              ),
-            ),
-          ),
+          _textFormField("Senha", "Digite a senha", isPassword: true),
           SizedBox(
             height: 20,
           ),
-          Container(
-            height: 46,
-            child: RaisedButton(
-              color: Colors.blue,
-              child: Text(
-                "Login",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 22,
-                ),
-              ),
-              onPressed: () {},
-            ),
-          )
+          _button("Login"),
         ],
+      ),
+    );
+  }
+
+  _button(String text) {
+    return Container(
+      height: 46,
+      child: RaisedButton(
+        color: Colors.blue,
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+          ),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  TextFormField _textFormField(String label, String hint,
+      {bool isPassword = false}) {
+    return TextFormField(
+      obscureText: isPassword,
+      style: TextStyle(
+        fontSize: 25,
+        color: Colors.blue,
+      ),
+      decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(
+          fontSize: 25,
+          color: Colors.grey,
+        ),
+        hintText: hint,
+        hintStyle: TextStyle(
+          fontSize: 16,
+        ),
       ),
     );
   }
