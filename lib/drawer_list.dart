b23a1521fbf5a6e1c5dd0fac35244dacadc43a38
Tharles Amaro <tharlesamaro@gmail.com>
2019-12-30 07:32:55 -0300

    Inserindo Drawer - menu lateral na Home

---
+++ lib/drawer_list.dart
@@ -0,0 +1,55 @@
+import 'package:carros/pages/login_page.dart';
+import 'package:carros/utils/navigator.dart';
+import 'package:flutter/material.dart';
+
+class DrawerList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Drawer(
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("Tharles Amaro"),
+              accountEmail: Text("tharlesamaro@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3Ss9W1G7soXGOGxfIxPZvOWqCF-LdxvO7a7RNpZggxZZDGoLK&s"),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.star),
+              title: Text("Favoritos"),
+              subtitle: Text("Mais informações..."),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                print("Item 1");
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.help),
+              title: Text("Ajuda"),
+              subtitle: Text("Mais informações..."),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                print("Item 2");
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text("Logout"),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () => _onCLickLogout(context),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _onCLickLogout(BuildContext context) {
+    Navigator.pop(context);
+    push(context, LoginPage(), replace: true);
+  }
+}
